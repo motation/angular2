34ab78e997bbf40b986237c6288ed85bb98ec068
Sven-Ole Fedders <Sven-Ole.Fedders@haw-hamburg.de>
2017-03-25 14:19:05 +0100

    added bootstrap and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { Navigation}  from './navigation/navigation.component'
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, Navigation ],
+  bootstrap:    [ AppComponent, Navigation ]
 })
 export class AppModule { }
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+/**
+ * Created by unknown on 25.03.17.
+ */
+
+@Component({
+    selector: 'navigation',
+    templateUrl: '/app/navigation/navigation.component.html'
+})
+
+export class Navigation {
+    private name:string = "navi";
+}
