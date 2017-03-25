a90e7a1b5010ab5831af043e29d7e89a0bfb8d88
Sven-Ole Fedders <Sven-Ole.Fedders@haw-hamburg.de>
2017-03-25 16:51:16 +0100

    added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { Navigation}  from './navigation/navigation.component'
 
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent }
+]
+
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent, Navigation ],
+  imports:      [ BrowserModule, RouterModule.forRoot(appRoutes) ],
+  declarations: [ AppComponent, Navigation, HomeComponent ],
   bootstrap:    [ AppComponent, Navigation ]
 })
 export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+/**
+ * Created by unknown on 25.03.17.
+ */
+
+@Component({
+  templateUrl:'/app/home/home.component.html'
+})
+
+export class HomeComponent{
+
+}
