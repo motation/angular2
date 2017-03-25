251b34e2ecb0e9e1d7ebc6136c3fe8c4bde835ae
Sven-Ole Fedders <Sven-Ole.Fedders@haw-hamburg.de>
2017-03-25 18:27:55 +0100

    wsocket added

--- backend/server.ts
+++ backend/server.ts
@@ -2,19 +2,58 @@
  * Created by unknown on 25.03.17.
  */
 import http = require('http');
-import fs = require('fs');
-import path = require('path');
 
 export class Server {
 
-    private static PORT: number = 3000;
+    private static PORT: number = 8081;
+    private static REQUEST: string = "request";
+    private server: any;
+    private websocketServer: any;
 
-    constructor() {
+    public start(): void {
+        this.server = http.createServer();
+        this.server.on(Server.REQUEST, this.onRequest)
+        this.server.listen(Server.PORT);
+
+        var Websocket = require('websocket').server;
+        this.websocketServer = new Websocket({
+            httpServer: this.server,
+            autoAcceptConnections: false
+        });
+
+        this.websocketServer.on('request', function (request: any) {
+            if (!this.originIsAllowed(request.origin)) {
+                // Make sure we only accept requests from an allowed origin
+                request.reject();
+                console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+                return;
+            }
+
+            var connection = request.accept('echo-protocol', request.origin);
+            console.log((new Date()) + ' Connection accepted.');
+            connection.on('message', function (message: any) {
+                if (message.type === 'utf8') {
+                    console.log('Received Message: ' + message.utf8Data);
+                    connection.sendUTF(message.utf8Data);
+                }
+                else if (message.type === 'binary') {
+                    console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+                    connection.sendBytes(message.binaryData);
+                }
+            });
+            connection.on('close', function (reasonCode: any, description: any) {
+                console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+            });
+        });
 
     }
 
-    public start(): void {
+    private originIsAllowed(origin: any): Boolean {
+        return true;
+    }
 
+    private onRequest = (request: any, response: any) => {
+        console.log(request.url);
     }
 }
 
---
+++ src/app/websocket/websocket.ts
@@ -0,0 +1,23 @@
+export class Websocket {
+    private connection: WebSocket;
+
+    constructor(port: string) {
+        this.connection = new WebSocket("ws://localhost:" + port, 'echo-protocol');
+    }
+
+    public addConnectionOpenCallback = function (callback: Function) {
+        this.connection.onopen = function (event: any) {
+            callback(event);
+        }
+    }
+
+    public send(message: any) {
+        this.connection.send(message);
+    }
+
+    public addReceiveCallback = function (callback: Function) {
+        this.connection.onmessage = function (message: any) {
+            callback(message);
+        }
+    }
+}
