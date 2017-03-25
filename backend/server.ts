61ff410ed8035fdc9e36e629943df0c670b497bb
Sven-Ole Fedders <Sven-Ole.Fedders@haw-hamburg.de>
2017-03-25 17:37:50 +0100

    backend + fixes

---
+++ backend/server.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by unknown on 25.03.17.
+ */
+import http = require('http');
+import fs = require('fs');
+import path = require('path');
+
+export class Server {
+
+    private static PORT: number = 3000;
+
+    constructor() {
+
+    }
+
+    public start(): void {
+
+    }
+}
+
+var server: Server = new Server();
+server.start();
