62405aa39e5ff452304d1579cdc0789486ee392a
Sven-Ole Fedders <fedders@office.silpion.de>
2017-03-30 16:11:39 +0200

    exercises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,20 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
 import {WebsocketService} from "./websocket/websocketService";
 import {ConfigurationComponent} from "./configuration/configuration.component";
+import {ExercisesComponent} from "./exercises/exercises.component";
 
 
 const appRoutes:Routes = [
     // { path: '**', component: HomeComponent },
     {path: 'home', component: HomeComponent},
-    {path: 'configuration', component: ConfigurationComponent}
+    {path: 'configuration', component: ConfigurationComponent},
+    {path: 'exercises', component: ExercisesComponent}
 ]
 
 
 @NgModule({
     imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
-    declarations: [AppComponent, Navigation, HomeComponent, ConfigurationComponent],
+    declarations: [AppComponent, Navigation, HomeComponent, ConfigurationComponent, ExercisesComponent],
     bootstrap: [AppComponent, Navigation],
     providers: [WebsocketService]
 })
---
+++ src/app/exercises/exercises.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by fedders on 30.03.2017.
+ */
+import {Component} from "@angular/core";
+@Component({
+    templateUrl: '/app/exercises/exercises.component.html'
+})
+export class ExercisesComponent {
+
+}
